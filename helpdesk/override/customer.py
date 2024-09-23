160f6e8f62a70a4c6a7c8e40eabfca1b8f2b0ba3
MBW-DatMinhNguyen <minhdat@mbw.vn>
2024-09-23 13:42:04 +0700

    feat[PM-5]: Tạo và theo dõi tài khoản người dùng

--- helpdesk/override/customer.py
+++ helpdesk/override/customer.py
@@ -39,3 +39,22 @@ class CustomCustomer(Customer):
         ]
         return {"columns": columns, "rows": rows}
 
+    def before_save(self):
+        if frappe.db.exists("User", {"email": self.email_id}):
+            # Nếu email đã tồn tại, hiển thị thông báo và không tạo mới
+            frappe.msgprint(f"Email {self.email_id} đã tồn tại trong User, không thể thêm mới User.")
+        # Kiểm tra nếu trường email_id rỗng
+        if not self.email_id:
+            # Hiển thị thông báo cho người dùng về việc thiếu email
+            frappe.throw("Không thể lưu khách hàng mới vì chưa có email. Vui lòng nhập địa chỉ email để tạo tài khoản đăng nhập cho khách hàng.")
+            
+    def after_insert(self) :
+        # Mục địch của việc viết vào hàm after_insert là để  khi người dùng tạo mới một bản ghi trong doctype Customer khi lưu sẽ sinh ra một bản ghi mới trong doctype User 
+        super(CustomCustomer, self).after_insert()
+        new_user = frappe.get_doc({
+            "doctype": "User",
+            "email": self.email_id or " ",
+            "enabled" : 0,
+            "first_name": self.customer_name or "Unknown"  # Giả sử có trường first_name
+        })
+        new_user.insert()
